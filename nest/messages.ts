df9aa4de337aa0027d04b73224e0ce087331f113
Nathally <nathisophi7@gmail.com>
2026-04-04 20:58:51 -0300

    Cria arquivo de mensagens estruturadas para o chatbot

---
+++ nest/messages.ts
@@ -0,0 +1,84 @@
+export const BotMessages = {
+  // O que estava no Word como: "Mensagem de boas vindas"
+SAUDACAO_INICIAL: "Olá!Bem-vindo ao atendimento Ouvidoria-EMAP. Para que possamos dar continuidade ao atendimento, me informe o seu nome e email",
+
+  // O que estava no Word como: "Menu principal"
+MENU_PRINCIPAL: `Para garantir a nossa comunicação, é importante que saiba que a cada interação, você tem o tempo de 1 hora para responder ou o chat será finalizado automaticamente.
+Por favor, escolha uma das opções abaixo digitando o número:
+1 - Visitas ao Porto
+2 - Trabalhe conosco
+3 - Oferta Comercial
+4 - Falar com Analista da Ouvidoria
+0 - Encerrar atendimento`,
+  
+OPCAO_INVALIDA: "Ops! Não entendi. Por favor, digite apenas o número da opção desejada.",
+
+    SUBMENU_VISITAS_PORTO: `O que você deseja Fazer?
+    1 - Voltar ao menu anterior
+    2 - Agendar visita ao porto
+    3 - Reagendar/Cancelar visita`,
+
+     //SE A PESSOA QUISER AGENDAR:
+        AGENDAR_VISITA_PORTO: `Agradecemos o interesse pela EMAP - Porto do Itaqui! ⚓️
+
+        📅 Agendamento de Visitas: É feito diretamente pelo nosso site: https://www.portodoitaqui.com/porto-do-itaqui/agende-uma-visita
+
+        ⚠️ Problemas no sistema? Caso o link apresente falha, fale com a nossa Gerência de Comunicação:
+        📞 Tel: (98) 3216-6020
+        ✉️ E-mail: comunicacao@emap.ma.gov.br
+
+        Atenciosamente,
+        Ouvidoria EMAP`,
+
+        //EM CASO DE REAGENDAR OU CANCELAMENTO
+        CANCELAR_VISITA_PORTO:`Com objetivo maior de atender a sua manifestação segue abaixo, contatos para melhores e maiores esclarecimentos de detalhes sobre a solicitação:
+
+        Gerência de Comunicação – GECOM
+        Tel.: (98)3216–6020
+        E-mail: comunicacao@emap.ma.gov.br
+
+        Estamos à disposição!`,
+
+    SUBMENU_TRABALHE_CONOSCO:`Informe o seu interesse:
+    1 - Voltar ao menu anterior
+    2 - Trabalhar conosco
+    3 - Estágio
+    4- Jovem Aprendiz`,
+    
+        TRABALHE_CONOSCO:`Primeiramente, gostaríamos de agradecer o seu contato e o interesse pela EMAP-Porto do Itaqui!
+  
+        A fim de atender a sua manifestação, informamos que a EMAP é uma empresa pública, administradora do Porto do Itaqui, e suas contratações são feitas por meio de concurso público. 
+ 
+        Contudo, caso esteja em busca de oportunidades, existem várias outras empresas privadas que atuam na área do Porto, conforme link: http://www.portodoitaqui.ma.gov.br/porto-do-itaqui/comunidade-portuaria. Para acompanhar informações sobre concursos, sugerimos que fique atento(a) ao site do Porto do Itaqui (https://www.portodoitaqui.com/emap/a-emap ).
+        Atenciosamente,
+        Ouvidoria EMAP.`,
+
+        ESTAGIO_PORTO:`Primeiramente, gostaríamos de agradecer o seu contato e o interesse pela EMAP-Porto do Itaqui!
+ 
+        A fim de atender a sua manifestação, informamos que, no caso de estágio na EMAP, os interessados devem se cadastrar no site do IEL - Instituto Euvaldo Lodi: http://sne.iel.org.br/ma. Quando surgem vagas, o recrutamento é feito por essa instituição, contratada pela EMAP como agente de integração.
+        Como seu foco é a área portuária, sugerimos que acesse o site  http://www.portodoitaqui.ma.gov.br/porto-do-itaqui/comunidade-portuaria para que você conheça mais desse universo e, ainda, possa encontrar outras empresas que atuam no Porto do Itaqui.
+        Atenciosamente,
+        Ouvidoria EMAP.`,
+
+        JOVEM_APRENDIZ_PORTO:`Primeiramente, gostaríamos de agradecer o seu contato e o interesse pela EMAP-Porto do Itaqui!
+
+        A fim de atender a sua manifestação, informamos que a empresa contrata jovens de 18 a 24 anos (que tenham concluído o ensino médio) por meio do Programa Jovem Aprendiz.
+
+        Para se candidatar a esse programa, você deve estar cadastrado na instituição formadora contratada pela empresa. Atualmente é o ISBET - Instituto Brasileiro Pró-Educação, Trabalho e Desenvolvimento ( www.isbet.org.br ). 
+        Atenciosamente,
+        Ouvidoria EMAP.`,
+
+    ANALISTA_OUVIDORIA:`Um atendente da EMAP entrará em contato em breve.`,
+
+// Quando a pessoa clicar em Oferta comercial.
+    RESPOSTA_COMERCIAL: `Agradecemos o interesse na EMAP. Por sermos uma empresa pública, nossas contratações ocorrem exclusivamente via licitação. Veja como acompanhar e participar:
+
+    📊 Editais: Acompanhe no nosso site (https://www.portodoitaqui.com/transparencia/licitacoes) e no programa PDF/FIEMA.
+    💻 Sistema: Nossos pregões ocorrem no portal Licitações-e do Banco do Brasil. É necessário se cadastrar lá: https://www.licitacoes-e.com.br/
+    📝 Cadastro de Fornecedores (CRC): Não é obrigatório para participar, mas facilita a entrega de documentos. Você pode fazer no site: https://www.portodoitaqui.com/emap/cadastro-de-fornecedores
+
+    Estamos à disposição!`,
+    
+DESPEDIDA: "Atendimento encerrado. A Ouvidoria - EMAP agradece o seu contato! Até logo! 👋"
+
+};
